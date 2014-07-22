09b329620f208930284101befa749ed2e8bf3dbe
Santorayo2 <santorayo@inpanic.de>
2014-07-22 18:04:45 +0200

    Tims Test Projekt juhu

---
+++ Tims Test Projekt/Assets/clickMe.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickMe : MonoBehaviour {
+
+	private float degrees = 0;
+
+	// Use this for initialization
+	void Start () {
+	Debug.Log ("Hello World");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		degrees = degrees + 25;
+		GameObject.FindGameObjectWithTag("Scheibe").transform.localRotation=Quaternion.Euler (0,degrees,0);
+
+	}
+}
